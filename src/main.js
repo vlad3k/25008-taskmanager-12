8e709c38d797f3524af2255438be8037cb014122
vlad3k <vlad3k@bk.ru>
2020-08-09 22:44:35 +0300

    feature: output data in templates

--- src/main.js
+++ src/main.js
@@ -4,9 +4,15 @@ import {createTaskTemplate} from "./view/task.js";
 import {createTaskEditTemplate} from "./view/task-edit.js";
 import {createLoadMoreButtonTemplate} from "./view/load-more-button.js";
 import {createBoardTemplate} from "./view/board.js";
-import {createSortTemplate} from "./view/sort";
+import {createSortTemplate} from "./view/sort.js";
+import {generateTask} from "./mock/task.js";
+import {generateFilter} from "./mock/filter";
 
-const TASK_COUNT = 3;
+const TASK_COUNT = 22;
+const TASK_COUNT_PER_STEP = 8;
+
+const tasks = new Array(TASK_COUNT).fill().map(generateTask);
+const filters = generateFilter(tasks);
 
 const render = (container, template, place = `beforeend`) => {
   container.insertAdjacentHTML(place, template);
@@ -16,18 +22,37 @@ const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 
 render(siteHeaderElement, createSiteMenuTemplate());
-render(siteMainElement, createFilterTemplate());
+render(siteMainElement, createFilterTemplate(filters));
 render(siteMainElement, createBoardTemplate());
 
 const boardElement = siteMainElement.querySelector(`.board`);
 const taskListElement = boardElement.querySelector(`.board__tasks`);
 
-render(taskListElement, createTaskEditTemplate());
+render(taskListElement, createTaskEditTemplate(tasks[0]));
 
 render(boardElement, createSortTemplate(), `afterbegin`);
 
-for (let i = 0; i < TASK_COUNT; i++) {
-  render(taskListElement, createTaskTemplate());
+for (let i = 1; i < Math.min(tasks.length, TASK_COUNT_PER_STEP); i++) {
+  render(taskListElement, createTaskTemplate(tasks[i]), `beforeend`);
 }
 
-render(boardElement, createLoadMoreButtonTemplate());
+if (tasks.length > TASK_COUNT_PER_STEP) {
+  let renderedTaskCount = TASK_COUNT_PER_STEP;
+
+  render(boardElement, createLoadMoreButtonTemplate(), `beforeend`);
+
+  const loadMoreButton = boardElement.querySelector(`.load-more`);
+
+  loadMoreButton.addEventListener(`click`, (evt) => {
+    evt.preventDefault();
+    tasks
+      .slice(renderedTaskCount, renderedTaskCount + TASK_COUNT_PER_STEP)
+      .forEach((task) => render(taskListElement, createTaskTemplate(task), `beforeend`));
+
+    renderedTaskCount += TASK_COUNT_PER_STEP;
+
+    if (renderedTaskCount >= tasks.length) {
+      loadMoreButton.remove();
+    }
+  });
+}
--- src/view/task.js
+++ src/view/task.js
@@ -1,18 +1,42 @@
-export const createTaskTemplate = () => {
+import {humanizeTaskDueDate, isTaskExpired, isTaskRepeating} from "../utils.js";
+
+export const createTaskTemplate = (task) => {
+  const {color, description, dueDate, repeating, isArchive, isFavorite} = task;
+
+  const date = dueDate !== null
+    ? humanizeTaskDueDate(dueDate)
+    : ``;
+
+  const deadlineClassName = isTaskExpired(dueDate)
+    ? `card--deadline`
+    : ``;
+
+  const repeatClassName = isTaskRepeating(repeating)
+    ? `card--repeat`
+    : ``;
+
+  const archiveClassName = isArchive
+    ? `card__btn--archive card__btn--disabled`
+    : `card__btn--archive`;
+
+  const favoriteClassName = isFavorite
+    ? `card__btn--favorites card__btn--disabled`
+    : `card__btn--favorites`;
+
   return (
-    `<article class="card card--black">
+    `<article class="card card--${color} ${deadlineClassName} ${repeatClassName}">
       <div class="card__form">
         <div class="card__inner">
           <div class="card__control">
             <button type="button" class="card__btn card__btn--edit">
               edit
             </button>
-            <button type="button" class="card__btn card__btn--archive">
+            <button type="button" class="card__btn ${archiveClassName}">
               archive
             </button>
             <button
               type="button"
-              class="card__btn card__btn--favorites card__btn--disabled"
+              class="card__btn card__btn--favorites ${favoriteClassName}"
             >
               favorites
             </button>
@@ -25,7 +49,7 @@ export const createTaskTemplate = () => {
           </div>
 
           <div class="card__textarea-wrap">
-            <p class="card__text">Example task with default color.</p>
+            <p class="card__text">${description}</p>
           </div>
 
           <div class="card__settings">
@@ -33,8 +57,8 @@ export const createTaskTemplate = () => {
               <div class="card__dates">
                 <div class="card__date-deadline">
                   <p class="card__input-deadline-wrap">
-                    <span class="card__date">23 September</span>
-                    <span class="card__time">16:15</span>
+                    <span class="card__date">${date}</span>
+                    <!-- <span class="card__time">16:15</span>-->
                   </p>
                 </div>
               </div>
